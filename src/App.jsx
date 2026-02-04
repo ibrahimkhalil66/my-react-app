7b1abac99c40223c231abe7f952f3101ad53435c
pc aounar <pcanouar@gmail.com>
2026-02-04 18:12:26 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route, Link } from "react-router-dom"
 import Home from './components/Home.jsx'
 import About from './components/About.jsx'
 
 function App() {
 
- 
-   return ( 
-      <>
-      <Home/>
-      <About/>
-      </>
+
+   return (
+      <div className="App">
+         <nav className="nav">
+            <Link to="/" className="nav-item">Home Page</Link>
+            <Link to="/about" className="nav-item">About Page</Link>
+         </nav>
+         <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+         </Routes>
+      </div>
    )
 
 }
